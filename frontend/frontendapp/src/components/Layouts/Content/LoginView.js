c7b82b622f8c4241f27fbf5ec1bd8e0d0b4db6df
Nhuttan1611 <nguyenhoangnhuttan1611@gmail.com>
2023-05-14 04:19:24 +0700

    login page

--- frontend/frontendapp/src/components/Layouts/Content/LoginView.js
+++ frontend/frontendapp/src/components/Layouts/Content/LoginView.js
@@ -31,54 +31,6 @@ class LoginView extends Component {
   render() {
     return (
       <>
-        <div className="hero_area">
-          <div className="bg-box">
-            <img
-              src="https://cdn.dribbble.com/userupload/3166340/file/original-7c0949addb967b19d37ee942722c1fc0.png?compress=1&resize=1024x768"
-              alt="None"
-            />
-          </div>
-          <section
-            className="book_section layout_padding"
-            style={{ padding: "100px" }}
-          >
-            <div id="wrapper">
-              <form className="form-login">
-                <h1 className="form-heading">Login</h1>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    name="username"
-                    className="form-input"
-                    placeholder="Username"
-                    value={this.state.credentials.username}
-                    onChange={this.inputChanged}
-                  ></input>
-                </div>
-                <div className="form-group">
-                  <input
-                    type="password"
-                    name="password"
-                    className="form-input"
-                    placeholder="Password"
-                    value={this.state.credentials.password}
-                    onChange={this.inputChanged}
-                  ></input>
-                </div>
-                <div className="form-register">
-                  <a href="/register" style={{color: "black"}}>Create Account</a>
-                </div>
-                <div className="form_container">
-                  <div className="btn_box">
-                    <div className="form-submit">
-                      <button onClick={this.login}>Login</button>
-                    </div>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </section>
-        </div>
       </>
     );
   }
